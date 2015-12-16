54247bacd214e69422bc801127a1eb0676efdd34
Lehner Florian <dev@der-flo.net>
2015-12-16 20:28:49 +0100

    0.3
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyOSCapi',
-    version='0.2',
+    version='0.3',
     py_modules=['pyOSCapi'],
     packages=find_packages(),
     include_package_data=True,
